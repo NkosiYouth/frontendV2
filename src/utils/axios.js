3ac5340433f5b5204999b72fa93c4d00e21aa9f1
NkosiYouth <nkosi@sayouthatwork.com>
2024-04-19 12:21:30 +0200

    render url update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
- axios.defaults.baseURL = 'https://backendv2-l5dj.onrender.com/';
+ axios.defaults.baseURL = 'https://backendv2-l5dj.onrender.com/api/';
 
 //axios.defaults.baseURL = ' http://127.0.0.1:5000/api';
 
